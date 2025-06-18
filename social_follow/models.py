46a5560aa83f7314dfe5aeedcc7a6b5eba5adec6
Mikee <islasmiguel737@gmail.com>
2025-06-17 19:16:13 -0600

    Fix Agregando correcion al modelo de la app de seguimiento

--- social_follow/models.py
+++ social_follow/models.py
@@ -3,41 +3,61 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from users.models import User
 
-class UserFollows(models.Model):
+class UserRelationship(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    
-    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower')
-    #Usuario que sigue a otro usuario
-    
-    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
-    #Usuario que esta siendo seguido
-    
+
+    follower = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following'
+    )
+    # Usuario que sigue a otro
+
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='followers'
+    )
+    # Usuario que está siendo seguido
+
     created_at = models.DateTimeField(auto_now_add=True)
     
+
     class Meta:
         db_table = 'user_follows'
-        unique_together = ('follower', 'following') #sirve para no estar duplicados los seguidores y seguidos
+        unique_together = ('follower', 'following')
         indexes = [
-            models.Index(fields=['follower', 'following']),
+            models.Index(fields=['follower']),
+            models.Index(fields=['following']),
+            
         ]
-        contrainst = [
+        constraints = [
             models.CheckConstraint(
-                check=models.Q(follower__ne=models.F('following')), 
+                check=~models.Q(follower=models.F('following')),
                 name='prevent_self_follow'
+                
             )
         ]
         verbose_name = 'Seguimiento de Usuario'
         verbose_name_plural = 'Seguimientos de Usuarios'
         
+
     def clean(self):
         if self.follower == self.following:
             raise ValidationError("Un usuario no puede seguirse a sí mismo.")
         
+
     def save(self, *args, **kwargs):
         self.clean()
         super().save(*args, **kwargs)
         
+
     def __str__(self):
         return f"{self.follower.username} sigue a {self.following.username}"
-        
     
+    
+class UserFollow(UserRelationship):
+    class Meta:
+        proxy = True
+        verbose_name = 'Alias de Seguimiento'
+        verbose_name_plural = 'Alias de Seguimientos'
