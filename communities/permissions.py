7e9f21db391f66c39d593d32bcb6d06f2984135d
Abraham <abrahamperezquiroz0@gmail.com>
2025-06-20 11:46:38 -0600

    Feat creacion de permissions y serializers para la app de comunidades

---
+++ communities/permissions.py
@@ -0,0 +1,30 @@
+from rest_framework import permissions
+from .models import CommunityMember
+
+class IsCommunityMember(permissions.BasePermission): #Esto es para que solo usuarios regristados se unan
+
+    def permission(self, request, view):  
+        community_id = view.kwargs.get('community_id')
+        if not community_id:
+            return False
+        return CommunityMember.objects.filter(
+            community_id=community_id,
+            user=request.user
+        ).exists()
+
+class IsCommunityModerator(permissions.BasePermission): #Esto es solo para los moderadores de las supercomunidades
+
+    def permission(self, request, view):
+        community_id = view.kwargs.get('community_id')
+        if not community_id:
+            return False
+
+        member = CommunityMember.objects.filter(
+            community_id=community_id,
+            user=request.user
+        ).select_related('role').first()
+
+        if not member:
+            return False
+
+        return member.role.permissions.get('can_moderate', True)
